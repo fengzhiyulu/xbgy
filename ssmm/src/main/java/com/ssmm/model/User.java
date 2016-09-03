c06c1f4e757e43755731a30ba30bf33459baccb9
Grubby <395282886@qq.com>
2016-09-04 00:05:50 +0800

    test

--- ssmm/src/main/java/com/ssmm/model/User.java
+++ ssmm/src/main/java/com/ssmm/model/User.java
@@ -4,52 +4,38 @@ import java.util.List;
 
 public class User {
 	private Integer id;
-
 	private String username;
-
 	private String password;
-
 	private Integer age;
-
-	private List<Book> books;
-
-	public List<Book> getBooks() {
-		return books;
-	}
-
-	public void setBooks(List<Book> books) {
-		this.books = books;
-	}
-
+	private List<Role> roles;
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getUsername() {
 		return username;
 	}
-
 	public void setUsername(String username) {
-		this.username = username == null ? null : username.trim();
+		this.username = username;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
-		this.password = password == null ? null : password.trim();
+		this.password = password;
 	}
-
 	public Integer getAge() {
 		return age;
 	}
-
 	public void setAge(Integer age) {
 		this.age = age;
+	}
+	public List<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
 	}
 }
