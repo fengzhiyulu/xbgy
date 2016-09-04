070cb5bb96510f250d77f7f6c126d14335265484
Grubby <395282886@qq.com>
2016-09-04 15:49:15 +0800

    ffdasf

--- ssmm/src/main/java/com/ssmm/controller/UserController.java
+++ ssmm/src/main/java/com/ssmm/controller/UserController.java
@@ -1,19 +1,10 @@
 package com.ssmm.controller;
 
-import java.util.Locale;
-
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.EnvironmentAware;
-import org.springframework.context.i18n.LocaleContext;
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ssmm.model.User;
 import com.ssmm.service.UserService;
@@ -34,16 +25,13 @@ public class UserController{
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(String username , String password ,Model model){
-		System.out.println();
+		User user = new User();
 		return "user/login";
 	}
 	
 	@RequestMapping(value = "/logout", method = RequestMethod.GET)
 	public String logout(Model model){
 		User user = new User();
-		user.setUsername("123");
-		user.setPassword("123");
-		userService.saveUser(user);
 		return "user/logout";
 	}
 }
